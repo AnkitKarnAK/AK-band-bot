a3d1941d0554a2c1babc15d099e93c2dc2b9abb6
ANKIT KARN <45123508+AnkitKarnAK@users.noreply.github.com>
2019-07-30 15:41:03 +0530

    Create band.js

---
+++ band.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const { prefix } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (command === 'ping') {
+        message.channel.send('Pong.');
+    }
+    else if (command === 'sync') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+        message.delete(1000);
+        message.channel.send(`=====\n<@&456550270701142058>\n\n\nSync time is out : ${args[0]}\nConfirm below if u can start or not:\n✅: YES\n❌: NO\n❕: Not sure`).then(function(message){message.react('✅')
+        .then(() => message.react('❌'))
+        .then(() => message.react('❕'))
+        .catch(() => console.error('One of the emojis failed to react.'))});
+        
+    }
+});
+
+
+client.login(token);
